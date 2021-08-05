424ae5c270a2760710584aa0581e9f8afdbe140c
Brendan Prednis <brendan.prednis@protonmail.com>
2021-08-05 15:54:26 -0700

    added hooks to my User Model.

--- models/user.ts
+++ models/user.ts
@@ -7,20 +7,39 @@ type UType = {
 };
 
 // create our User table
-const User = connection.define("User", {
-  uuid: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    autoIncrement: true,
-    primaryKey: true,
+const User = connection.define(
+  "User",
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    first: {
+      type: DataTypes.STRING,
+    },
+    last: {
+      type: DataTypes.STRING,
+    },
+    full_name: DataTypes.STRING,
+    bio: DataTypes.TEXT,
   },
-  name: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-  },
-  bio: {
-    type: DataTypes.TEXT,
-  },
-});
+  {
+    hooks: {
+      beforeValidate: () => {
+        console.log("before");
+      },
+      afterValidate: () => {
+        console.log("after validate");
+      },
+      beforeCreate: () => {
+        console.log("before create");
+      },
+      afterCreate: () => {
+        console.log("after create");
+      },
+    },
+  }
+);
 // export out User
 export default User;
--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize";
+import express from "express";
 
 // utils
 import connection from "./util/database";
@@ -6,6 +7,24 @@ import connection from "./util/database";
 // models
 import User from "./models/user";
 
+const PORT = 8000;
+const app = express();
+
+// routes
+
+// app.get("/", (_, res) => {
+//   User.create({
+//     name: "brendi",
+//     bio: "nice",
+//   })
+//     .then((user) => {
+//       res.json(user);
+//     })
+//     .catch((err) => {
+//       res.status(404).send(err);
+//     });
+// });
+//
 // sync will create through our models.
 connection
   .sync({ force: true })
@@ -14,10 +33,15 @@ connection
   })
   .then(() => {
     User.create({
-      name: "Brendan",
-      bio: "man I wish this worked",
+      first: "brendi",
+      last: "prendo",
+      full_name: "brendo prendo",
+      bio: "new bio here",
     });
   })
   .catch((err) => {
     console.log(err);
   });
+
+// application
+app.listen(PORT, () => console.log(`transPORT:${PORT} is away`));
